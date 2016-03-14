a1cc843c9ac8711eb2cb1a0acc7a92386d8052de
Johnil Quezada <johnil.quezada94@gmail.com>
2016-03-14 12:53:56 -0400

    Lab2

--- Lab1/Restaurant/app/controllers/pages_controller.rb
+++ Lab1/Restaurant/app/controllers/pages_controller.rb
@@ -1,13 +1,27 @@
 class PagesController < ApplicationController
 
-	
+	API_KEY = "AIzaSyDeRjJ-3FOHFDYWlEAkwv_CQEpkoeA7azw"
+	BASE_URL = "https://maps.googleapis.com/maps/api/staticmap?zoom=15&size=700x300"
+	PIPE_URL_CHARACTER = "%7C"
+	MARKER_PARAMETER = "&markers="
+	KEY_PARAMETER = "&key=" + API_KEY
 
 	def index
 		@restaurants = Restaurant.all
+		@markers = ""
+		@restaurants.each do |restaurant|
+			@address = restaurant.address.gsub!(' ', '%20')
+			@markers+= MARKER_PARAMETER + restaurant.address
+		end
+		@mapUrl = BASE_URL + @markers + KEY_PARAMETER
+		puts @mapUrl
+
 	end
 
 	def show
 		@restaurant = Restaurant.find(params[:id])
+		@address = @restaurant.address.gsub(' ', '%20')
+		@mapUrl = BASE_URL + MARKER_PARAMETER + @address + KEY_PARAMETER
 	end
 
 	def new
